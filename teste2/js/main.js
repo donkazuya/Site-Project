433ab9389499b76e645fc7d140537e56a23b10da
Danrley Batista Candido da Silva <danrley.tri@gmail.com>
2018-04-02 16:26:49 -0300

    add Carousel

--- teste2/js/main.js
+++ teste2/js/main.js
@@ -1,11 +1,19 @@
 jQuery(document).ready(function($) {
-	//botão moblie
+	//Menu Burger Open
 	$('.burger').click(function () {
-  $(this).toggleClass('open');
+  		$(this).toggleClass('open');
 });
+	//Show Menu
 	$('.burger').click(function(event) {
 		$('.menu-mobile').slideToggle();	
 	});
-	//esconder menu
 	
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000
+	});
 });
