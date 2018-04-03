ca72cd3f21ff2069c460370bbaea5c2d237a0c49
Danrley Batista Candido da Silva <danrley.tri@gmail.com>
2018-04-03 13:28:31 -0300

    add carousel-bottom

--- teste2/js/main.js
+++ teste2/js/main.js
@@ -2,18 +2,47 @@ jQuery(document).ready(function($) {
 	//Menu Burger Open
 	$('.burger').click(function () {
   		$(this).toggleClass('open');
-});
+	});
+
 	//Show Menu
 	$('.burger').click(function(event) {
 		$('.menu-mobile').slideToggle();	
 	});
 	
-	$(".owl-carousel").owlCarousel({
+
+	$("#carousel-main .owl-carousel").owlCarousel({
 		items: 1,
 		loop: true,
 		dots: true,
 		autoplay: true,
 		autoplayTimeout: 4000,
-		smartSpeed: 1000
+		smartSpeed: 1000,
+		responsive: {
+			0:{
+				nav: false
+			},
+			768:{
+				nav: true
+			}
+		}
 	});
+
+	$(".portfolio-text .owl-carousel").owlCarousel({
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000,
+		nav: true,
+		responsive : {
+		    // breakpoint from 0 up
+		    0 : {
+		        items:1
+		    },
+		    768 : {
+		        items: 3
+		    }
+		}
+	});
+
+	
 });
