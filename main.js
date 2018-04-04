9545fb687668b5955c417846383c33ed57e0e82d
Danrley Batista Candido da Silva <danrley.tri@gmail.com>
2018-04-04 11:52:25 -0300

    add mouseDrag

---
+++ main.js
@@ -0,0 +1,49 @@
+jQuery(document).ready(function($) {
+	//Menu Burger Open
+	$('.burger').click(function () {
+  		$(this).toggleClass('open');
+	});
+
+	//Show Menu
+	$('.burger').click(function(event) {
+		$('.menu-mobile').slideToggle();	
+	});
+	
+
+	$("#carousel-main .owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000,
+		touchDrag: false,
+		mouseDrag: false,
+		responsive: {
+			0:{
+				nav: false
+			},
+			768:{
+				nav: true
+			}
+		}
+	});
+
+	$(".portfolio-text .owl-carousel").owlCarousel({
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		smartSpeed: 1000,
+		responsive : {
+		    // breakpoint from 0 up
+		    0 : {
+		        items:1
+		    },
+		    768 : {
+		        items: 3
+		    }
+		}
+	});
+
+	
+});
