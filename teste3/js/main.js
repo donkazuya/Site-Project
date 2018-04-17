aa68ae1a7f6fef80a483970619585d9f5b663f06
Danrley Batista Candido da Silva <danrley.tri@gmail.com>
2018-04-17 16:27:35 -0300

    add responsive

--- teste3/js/main.js
+++ teste3/js/main.js
@@ -3,7 +3,43 @@ $(window).on('load', function() {
 		$(this).remove();
 	});
 });
+
+function contador() {
+	$('.count').each(function () {
+          $(this).prop('Counter',0).animate({
+              Counter: $(this).text()
+          }, {
+              duration: 6000,
+              easing: 'swing',
+              step: function (now) {
+                  $(this).text(Math.ceil(now));
+            }
+         });
+     });
+}
+
+$(window).scroll(function() {
+   var hT = $('#facts').offset().top,
+       hH = $('#facts').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+     contador(); 
+   } 
+});
+
 jQuery(document).ready(function($){
+
+		//Menu Burger Open
+		$('.burger').click(function () {
+	  		$(this).toggleClass('open');
+		});
+
+		//Show Menu
+		$('.burger').click(function(event) {
+			$('.menu-mobile').slideToggle();	
+		});
+
 	$('#inicio').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
@@ -14,18 +50,28 @@ jQuery(document).ready(function($){
 	$('.equipe').slick({
 		slidesToShow: 4,
 		slidesToScroll: 1,
-		infinite: false
+		infinite: false,
+		responsive: [
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        arrows: false,
+		        slidesToShow: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 1,
+		        infinite: true
+		      }
+		    }
+		  ]
 	});
 
-	$('.count').each(function () {
-	    $(this).prop('Counter',0).animate({
-	        Counter: $(this).text()
-	    }, {
-	        duration: 6000,
-	        easing: 'swing',
-	        step: function (now) {
-	            $(this).text(Math.ceil(now));
-	        }
-	    });
-	});
+
+
 });
